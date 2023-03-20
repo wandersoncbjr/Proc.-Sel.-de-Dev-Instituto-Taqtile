437b6cdbc6a2eeb7d4e7f4085167996771b05d96
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-03-20 19:32:52 -0300

    Correção de escrita no Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Word🌍</h1>
+        <h1>Hello World🌍</h1>
       </header>
     </div>
   );
