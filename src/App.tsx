37d142633c084511f091b123ae27cae2f0f54814
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-03-22 17:49:43 -0300

    Add Prettier and Eslint to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Hello World🌍</h1>
       </header>
     </div>
