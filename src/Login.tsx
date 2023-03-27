7ce86a665368dcdb110076f13ca1f204278c68dc
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-03-27 17:56:53 -0300

    add kebab-case

--- src/Login.tsx
+++ src/Login.tsx
@@ -2,16 +2,16 @@ import './Login.css';
 
 function Login() {
   return (
-    <div className='Login'>
-      <form method='POST' className='formLogin'>
-        <header className='Login-header'>
+    <div className='login-container'>
+      <form method='POST' className='form-login'>
+        <header>
           <h1>Bem vindo(a) à Taqtile!</h1>
         </header>
         <label htmlFor='email'>E-mail</label>
         <input type='email' placeholder='Digite seu e-mail' />
         <label htmlFor='password'>Senha</label>
         <input type='password' placeholder='Digite seu e-mail' />
-        <input type='submit' value='Acessar' className='btn' />
+        <input type='submit' value='Acessar' className='btn-login' />
       </form>
     </div>
   );
