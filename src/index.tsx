153e8e582cca2aeec61c5bf0dc78dede56086f30
Wanderson Santos <wandersoncbjr2@gmail.com>
2023-03-31 19:37:20 -0300

    add: route logado

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Login';
+import Login from './components/login/Login';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Logado from './components/logado';
 
 const client = new ApolloClient({
   uri: 'https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql',
   cache: new InMemoryCache(),
 });
 
+const router = createBrowserRouter([
+  { path: '/login', element: <Login /> },
+  { path: '/logado', element: <Logado /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Login />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>,
 );
